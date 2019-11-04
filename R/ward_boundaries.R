11f2969296c31d2bceed9d5706f9583548d7fceb
Joseph Oladokun <37074399+Godskid89@users.noreply.github.com>
2019-11-04 10:29:45 +0100

    adding functions

---
+++ R/ward_boundaries.R
@@ -0,0 +1,6 @@
+# ---- Ward Admin Boundaries
+ward_admin_boundaries <- function() {
+  #Pull data from eHealth Africa geoserver
+  data <- read.csv("https://gis-a.ie.ehealthafrica.org/geoserver/eHA_db/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=eHA_db:sv_wards&outputFormat=csv")
+  print(data)
+}
